fabbceb737ec2f6cdf5fc34b6d04f532709e87a0
Mitja Krašovec <mitjakrasovec1@gmail.com>
2021-09-09 19:36:26 +0200

    Added Bootstrap, started first page, added basic CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StepOneComponent } from './step-one/step-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepOneComponent
   ],
   imports: [
     BrowserModule,
